b7ea8c17c6dbccfb4acdb40c782fcb364ee2e9a6
sajidthedev <a.dev.inside@gmail.com>
2025-01-21 13:14:36 +0500

    Structural Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
 import { ListEmployeeComponent } from './list-employee/list-employee.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { StructuralDirectiveComponent } from './structural-directive/structural-directive.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AddEmployeeComponent, ListEmployeeComponent , DataBindingComponent],
+  imports: [RouterOutlet, AddEmployeeComponent, ListEmployeeComponent , DataBindingComponent ,StructuralDirectiveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/structural-directive/structural-directive.component.ts
@@ -0,0 +1,54 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-structural-directive',
+  imports: [CommonModule , FormsModule],
+  templateUrl: './structural-directive.component.html',
+  styleUrl: './structural-directive.component.css'
+})
+export class StructuralDirectiveComponent {
+
+  isVisibleDiv1 = true;
+  isVisibleDiv2 = true;
+  num1: string ='';
+  num2: string = '';
+  isActive= false;
+  selectedState = '';
+
+  cityArray:string[] = ['Lahore', 'Fsd', 'Karchi', 'Okara', 'ISd'];
+
+  // object of array
+
+  studentList:any[]=[
+    {id:1, name:'Ali', city:'Lahore' , age: 20},
+    {id:2, name:'Ahmed', city:'Fsd' , age: 22},
+    {id:3, name:'Asad', city:'Karchi', age: 24},
+    {id:4, name:'Ahsan', city:'Okara', age: 26},
+    {id:5, name:'Asim', city:'ISd', age: 28},
+  ]
+
+
+
+  showDiv1(){
+    this.isVisibleDiv1 = true;
+  }
+  hideDiv1(){
+    this.isVisibleDiv1 = false;
+  }
+
+  toggleDiv2(){
+    this.isVisibleDiv2 = !this.isVisibleDiv2;
+    if(this.isVisibleDiv2==true)
+{
+  this.isVisibleDiv2 = false;
+  console.log("Div2 is visible");
+}
+else{
+  this.isVisibleDiv2 = true;
+  console.log("Div2 is not visible");
+}
+  }
+
+}
